a10385e79f07591c667eca2390ea1ebd83edac2a
abalozz <abalozz@outlook.com>
2019-08-03 18:43:55 +0200

    Eliminar código CSS y JS que no se utiliza.

--- src/js/index.js
+++ src/js/index.js
@@ -6,46 +6,10 @@ function documentReady(fn) {
   }
 }
 
-function interval (fn, t) {
-  return window.setInterval(fn, t);
-}
-
-
-;(function (window, document, undefined) {
+;(function (document) {
   "use strict";
 
   documentReady(function () {
-
-    var scrollAnchors = document.querySelectorAll('[data-scroll-down]');
-    if (scrollAnchors) {
-      [].forEach.call(scrollAnchors, function (element) {
-        element.addEventListener('click', function (event) {
-          event.preventDefault();
-
-          var href = element.getAttribute('href');
-          var elementToScroll = document.querySelector(href);
-          var positionToScroll = elementToScroll.offsetTop - 3;
-          var actualScroll = window.scrollY;
-          var direction = (actualScroll > positionToScroll) ? -1 : 1;
-          var duration = 300;
-          var fraction = 10;
-          var scrollBy = (positionToScroll - actualScroll) * direction / (duration / fraction);
-          var time = 0;
-
-          var scrollAnimated = function () {
-            window.scrollBy(0, scrollBy);
-            time += fraction;
-            if (time >= duration) {
-              clearInterval(intervalID);
-              window.scrollTo(window.scrollX, positionToScroll);
-            }
-          };
-
-          var intervalID = interval(scrollAnimated, fraction);
-        });
-      });
-    }
-
     Prism.highlightAll();
 
     var times = document.querySelectorAll('time');
@@ -60,7 +24,5 @@ function interval (fn, t) {
 
       time.textContent = content;
     });
-
   });
-
-})(window, document);
+})(document);
