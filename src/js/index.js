d3ecc8f87dac637e7d97fe294f79a19a756cfa2d
abalozz <abalozz@outlook.com>
2019-08-03 19:06:10 +0200

    Eliminar traducción de fechas, que ya lo hace Ghost desde hace tiempo.

--- src/js/index.js
+++ src/js/index.js
@@ -6,23 +6,6 @@ function documentReady(fn) {
   }
 }
 
-;(function (document) {
-  "use strict";
-
-  documentReady(function () {
-    Prism.highlightAll();
-
-    var times = document.querySelectorAll('time');
-    [].forEach.call(times, function (time) {
-      var englishMonths = 'January February March April May June July August September October November December'.split(' ');
-      var spanishMonths = 'Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre'.split(' ');
-      var content = time.textContent;
-
-      englishMonths.forEach(function (month, index) {
-        content = content.replace(month, spanishMonths[index]);
-      });
-
-      time.textContent = content;
-    });
-  });
-})(document);
+documentReady(function () {
+  Prism.highlightAll();
+});
