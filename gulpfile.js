3fbff63f93cf78720e6e9fac1899525c1891726a
abalozz <abalozz@outlook.com>
2019-08-03 14:28:50 +0200

    Actualización de Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,14 @@
 
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
-const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const mergeStream = require('merge-stream');
-const path = require('path');
 
-gulp.task('build:css', function () {
+function buildCss() {
     const stylusStream = gulp.src('src/stylus/index.styl').pipe(stylus());
     const cssStream = gulp.src('node_modules/prismjs/themes/prism-okaidia.css');
 
@@ -27,9 +25,9 @@ gulp.task('build:css', function () {
         .pipe(postcss(plugins))
         .pipe(concat('blog.css'))
         .pipe(gulp.dest('assets/'));
-});
+}
 
-gulp.task('build:js', function () {
+function buildJs() {
     let jsFiles = [
         'node_modules/prismjs/prism.js',
         'node_modules/prismjs/components/prism-bash.js',
@@ -39,6 +37,8 @@ gulp.task('build:js', function () {
         'node_modules/prismjs/components/prism-git.js',
         'node_modules/prismjs/components/prism-javascript.js',
         'node_modules/prismjs/components/prism-json.js',
+        'node_modules/prismjs/components/prism-js-extras.js',
+        'node_modules/prismjs/components/prism-js-templates.js',
         'node_modules/prismjs/components/prism-jsx.js',
         'node_modules/prismjs/components/prism-markdown.js',
         'node_modules/prismjs/components/prism-markup.js',
@@ -53,20 +53,24 @@ gulp.task('build:js', function () {
         .pipe(concat('blog.js'))
         .pipe(uglify())
         .pipe(gulp.dest('assets/'));
-});
+}
 
-gulp.task('build', ['build:css', 'build:js']);
+function reload(done) {
+    browserSync.reload();
+    done();
+}
 
-gulp.task('watch', function () {
-    gulp.watch('src/stylus/**/*.styl', ['build:css', browserSync.reload]);
-    gulp.watch('src/js/**/*.js', ['build:js', browserSync.reload]);
-});
-
-gulp.task('browser-sync', function() {
-    browserSync({
+function sync() {
+    browserSync.init({
         proxy: 'http://localhost:2368',
         open: false,
     });
-});
+}
 
-gulp.task('default', ['build', 'watch', 'browser-sync']);
+function watch() {
+    gulp.watch('**/*.hbs', reload);
+    gulp.watch('src/stylus/**/*.styl', gulp.series(buildCss, reload));
+    gulp.watch('src/js/**/*.js', gulp.series(buildJs, reload));
+}
+
+exports.default = gulp.parallel(buildCss, buildJs, watch, sync);
